7b6b8d3e42d76ed046b0b6934997dbf5b66e52ce
Abdulla-ak1702167 <abdolla-tareq@hotmail.com>
2022-12-31 19:14:27 +0300

    Update students_screen.dart

--- lib/screens/student_screen/students_screen.dart
+++ lib/screens/student_screen/students_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:taqeem/model/student.dart';
 
 import 'edit_student.dart';
 
@@ -12,46 +11,8 @@ class StudentsScreen extends StatefulWidget {
 }
 
 class _StudentsScreenState extends State<StudentsScreen> {
-  var _dropdownValue;
-
-  dropdownCallback(value) {
-    setState(() {
-      _dropdownValue = value;
-    });
-  }
-
   final Stream<QuerySnapshot> _studentsStream =
       FirebaseFirestore.instance.collection('students').snapshots();
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return StreamBuilder<QuerySnapshot>(
-  //     stream: _studentsStream,
-  //     builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-  //       if (snapshot.hasError) {
-  //         return const Text('Something went wrong');
-  //       }
-
-  //       if (snapshot.connectionState == ConnectionState.waiting) {
-  //         return const Text("Loading");
-  //       }
-
-  //       return ListView(
-  //         children: snapshot.data!.docs
-  //             .map((DocumentSnapshot document) {
-  //               Map<String, dynamic> data =
-  //                   document.data()! as Map<String, dynamic>;
-  //               return ListTile(
-  //                 title: Text(data['name']),
-  //                 subtitle: Text(data['score'].toString()),
-  //               );
-  //             })
-  //             .toList()
-  //             .cast(),
-  //       );
-  //     },
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -68,30 +29,44 @@ class _StudentsScreenState extends State<StudentsScreen> {
           );
         },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButton(
-              value: _dropdownValue,
-              items: const [
-                DropdownMenuItem(
-                  value: "data",
-                  child: Text("data"),
-                ),
-                DropdownMenuItem(
-                  value: "data1",
-                  child: Text("data1"),
-                ),
-                DropdownMenuItem(
-                  value: "data11",
-                  child: Text("data11"),
-                ),
-              ],
-              onChanged: dropdownCallback,
-            ),
-          ],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _studentsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
+                  return Card(
+                      child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    ListTile(
+                      leading: const Icon(
+                        Icons.person,
+                        size: 40.0,
+                        color: Colors.indigo,
+                      ),
+                      trailing: const Icon(
+                        Icons.more_horiz,
+                        size: 20.0,
+                        color: Colors.indigo,
+                      ),
+                      title: Text(data['name']),
+                      subtitle: Text(data['score'].toString()),
+                    )
+                  ]));
+                })
+                .toList()
+                .cast(),
+          );
+        },
       ),
     );
   }
